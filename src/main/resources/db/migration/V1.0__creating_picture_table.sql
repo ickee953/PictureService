9c13b3b52b9d965ca303dff97b35308d17212902
usr <usr@localhost.localdomain>
2024-07-03 14:07:18 +0300

    Flyway sql if not exists.

--- src/main/resources/db/migration/V1.0__creating_picture_table.sql
+++ src/main/resources/db/migration/V1.0__creating_picture_table.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE picture (
+CREATE TABLE IF NOT EXISTS picture (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     path VARCHAR(100) NOT NULL,
     created_at TIMESTAMP NOT NULL
