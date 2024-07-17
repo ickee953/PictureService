b4555c9eea1d0b930defbf22b2329b6005b5ed72
ickee953 <ickee953@gmail.com>
2024-07-17 13:33:56 +0300

    Move topics to properties.

--- src/main/java/com/github/ickee953/micros/pictures/messages/UploadFileConsumer.java
+++ src/main/java/com/github/ickee953/micros/pictures/messages/UploadFileConsumer.java
@@ -7,17 +7,13 @@ import com.github.ickee953.micros.pictures.service.DefaultPictureService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @Component
@@ -25,32 +21,36 @@ import java.util.concurrent.ExecutionException;
 @RequiredArgsConstructor
 public class UploadFileConsumer {
 
-    private static final String KAFKA_TOPIC = "file-uploaded-topic";
-
     private final KafkaTemplate<String, String> kafkaTemplate;;
 
-    private final DefaultPictureService pictureService;
+    @Value(value = "${app.kafka.topic.file-uploaded}")
+    private String fileUploadedTopic;
 
-    @KafkaListener(topics = {"file-upload-topic"})
+    private final DefaultPictureService fileService;
+
+    @Value(value = "${app.default-upload-filename}")
+    private String defaultFileName;
+
+    @KafkaListener(topics = {"${app.kafka.topic.file-upload}"})
     public void uploadFile(ConsumerRecord<String, byte[]> record){
         log.info("received message : {}", record);
 
         assert record.value() != null;
 
-        MultipartFile file = new Base64DecodedMultipartFile("pic.jpg", record.value());
+        MultipartFile file = new Base64DecodedMultipartFile(defaultFileName, record.value());
 
-        List<String> uploadedFilesPaths = pictureService.uploadFiles(List.of(file));
+        List<String> uploadedFilesPaths = fileService.uploadFiles(List.of(file));
 
         uploadedFilesPaths.forEach( url -> {
             PictureDto dto = new PictureDto(url);
 
-            Picture picture = pictureService.create( dto );
+            Picture picture = fileService.create( dto );
 
             try {
-                kafkaTemplate.send(KAFKA_TOPIC, record.key(), picture.getPath() ).get();
+                kafkaTemplate.send(fileUploadedTopic, record.key(), picture.getPath() ).get();
             } catch (InterruptedException | ExecutionException e) {
                 log.error(String.format("Failed to send uploaded picture kafka message in topic: %s with key: %s",
-                        KAFKA_TOPIC, picture.getId()));
+                        fileUploadedTopic, picture.getId()));
             }
         } );
     }
